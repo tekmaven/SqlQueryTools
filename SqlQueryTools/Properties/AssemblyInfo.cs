298555ac398e36ac8d6818dfb070466148b0ad3e
Ryan Hoffman <rhoffman@extended64.com>
2012-04-30 23:41:18 -0400

    Renamed solution to SqlQueryTools, to match project name

--- SqlQueryTools/Properties/AssemblyInfo.cs
+++ SqlQueryTools/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SqlTools")]
+[assembly: AssemblyTitle("SqlQueryTools")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SqlTools")]
+[assembly: AssemblyProduct("SqlQueryTools")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
